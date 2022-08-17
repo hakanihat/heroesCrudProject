9b104ea90925fe1d3f5bbe17e951ced901ec7788
hakanihat <62997624+hakanihat@users.noreply.github.com>
2022-08-18 00:41:09 +0300

    crud project init

---
+++ heroesCrudProject/src/main/java/com/example/heroescrudproject/HeroesCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.heroescrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesCrudProjectApplication.class, args);
+	}
+
+}
---
+++ heroesCrudProject/src/test/java/com/example/heroescrudproject/HeroesCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heroescrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
